e83134d58c86427553cb5fbfdaeb1865321271f6
YevheniiLutsk <113389821+YevheniiLutsk@users.noreply.github.com>
2023-05-10 18:21:01 +0300

    fix

--- assembler/gulp/config/plugins.js
+++
@@ -1,9 +0,0 @@
-import notify from 'gulp-notify';
-import plumber from 'gulp-plumber';
-import gulpIf from 'gulp-if';
-
-export const plugins = {
-    notify:notify,
-    plumber:plumber,
-    if:gulpIf
-}
